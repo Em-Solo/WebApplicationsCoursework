e7c163abf3e7a682e5dea2f2a763ceaaac4c0420
aimilios <as03382@surrey.ac.uk>
2021-12-01 22:36:31 +0000

    Added footer header and navbar

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   root 'home#home'
   get 'contact', to:'home#contact'
   post 'request_contact', to: 'home#request_contact'
-  
+
+  get 'home', to: 'home#home'
 end
