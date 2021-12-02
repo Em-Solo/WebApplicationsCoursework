796c0029fa5ba346a15fa7fea73d9a430d6c4401
aimilios <as03382@surrey.ac.uk>
2021-12-02 21:50:08 +0000

    Added index on the titles of the movies in the movies table

--- db/migrate/20211202211548_create_movies.rb
+++ db/migrate/20211202211548_create_movies.rb
@@ -1,7 +1,7 @@
 class CreateMovies < ActiveRecord::Migration[5.2]
   def change
     create_table :movies do |t|
-      t.string :title
+      t.string :title, null: false
       t.string :genre
       t.integer :runtime
       t.date :watch_date
