6ef7640f30e1b4d586618241b5413cd0a73c1556
aimilios <as03382@surrey.ac.uk>
2021-12-02 21:23:33 +0000

    Movie scaffold

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :movies
   root 'home#home'
   get 'contact', to:'home#contact'
   post 'request_contact', to: 'home#request_contact'
---
+++ db/migrate/20211202211548_create_movies.rb
@@ -0,0 +1,13 @@
+class CreateMovies < ActiveRecord::Migration[5.2]
+  def change
+    create_table :movies do |t|
+      t.string :title
+      t.string :genre
+      t.integer :runtime
+      t.date :watch_date
+      t.integer :score
+
+      t.timestamps
+    end
+  end
+end
