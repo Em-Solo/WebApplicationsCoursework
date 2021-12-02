f686621edad6ebdf087fb9ff5787d44509dfe778
aimilios <as03382@surrey.ac.uk>
2021-12-02 22:34:10 +0000

    Added welcome seed for database

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+welcome = Movie.where(title: 'Welcome').first
+welcome.delete if welcome
+
+movies = Movie.create([title:'Welcome', genre:'Comedy', runtime:999, watch_date:'01/12/2021', score:100])
