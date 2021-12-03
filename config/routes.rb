27e5fb701ff921e11b1ebe05425835ce8825e31e
aimilios <as03382@surrey.ac.uk>
2021-12-03 15:41:06 +0000

    Fixed tests and made the style slightly better

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 Rails.application.routes.draw do
   resources :movies
   root 'home#home'
+  get 'home', to: 'home#home'
   get 'contact', to:'home#contact'
   post 'request_contact', to: 'home#request_contact'
-
-  get 'home', to: 'home#home'
 end
--- test/models/movie_test.rb
+++ test/models/movie_test.rb
@@ -1,7 +1,45 @@
 require 'test_helper'
 
 class MovieTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  test 'should not save empty movie' do
+    movie = Movie.new
+
+    movie.save
+    refute movie.valid?
+  end
+
+  test 'should save valid note' do
+    movie = Movie.new
+
+    movie.title = 'Home Alone'
+    movie.genre = 'Family'
+    movie.runtime = 100
+    movie.watch_date = '03/12/2021'
+    movie.score = 66
+
+    movie.save
+    assert movie.valid?
+  end
+
+  test 'should not save duplicate movie title' do
+    movie1 = Movie.new
+    movie1.title = 'Clueless'
+    movie1.genre = 'Romance'
+    movie1.runtime = 80
+    movie1.watch_date = '02/12/2021'
+    movie1.score = 85
+    movie1.save
+    assert movie1.valid?
+
+    movie2 = Movie.new
+    movie2.title = 'Clueless'
+    movie2.genre = 'Romance'
+    movie2.runtime = 80
+    movie2.watch_date = '02/12/2021'
+    movie2.score = 85
+    movie2.save
+    refute movie2.valid?
+  end
+
 end
