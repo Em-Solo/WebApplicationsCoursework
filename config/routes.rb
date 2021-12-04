91450aad5c5c6b7ed112a4715b8efaa84b87d7b7
aimilios <as03382@surrey.ac.uk>
2021-12-04 21:46:35 +0000

    Added Devise and improved some styling

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,16 @@
 Rails.application.routes.draw do
+  devise_for :users
+  # Resource paths.
   resources :reviews
   resources :movies
+
+  # Home page is the root.
   root 'home#home'
+
+  #Route to home page.
   get 'home', to: 'home#home'
+
+  # Contact Page.
   get 'contact', to:'home#contact'
   post 'request_contact', to: 'home#request_contact'
 end
